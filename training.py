c76c0df42636c20ada0cd6eb28b55333b7687793
Farzad FARID <farzy@farzy.org>
2020-05-16 16:42:54 +0200

    Add more formatting examples and __str__ on a class

--- training.py
+++ training.py
@@ -19,6 +19,23 @@ def strings():
     print("{2}, {1}, {0}".format(*'abc'))
     print("{0}{1}{0}".format('abra', 'cad'))
 
+    print("Coordinates: {latitude}, {longitude}".format(latitude='37.24N', longitude='-115.81W'))
+    coord = {'latitude': '37.24N', 'longitude': '-115.81W'}
+    print("Coordinates: {latitude}, {longitude}".format(**coord))
+
+    c = 3-5j
+    print("The complex number {0} if formed of the real part {0.real} and the imaginary part {0.imag}".format(c))
+
+    class Point:
+        def __init__(self, x, y):
+            self.x, self.y = x, y
+
+        def __str__(self):
+            return "Point({self.x}, {self.y})".format(self=self)
+
+    p = Point(2, 3)
+    print("Object {0!r} = {0}".format(p))
+
 
 def main():
     """The main program call the training sessions"""
