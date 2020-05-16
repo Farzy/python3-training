394d7d0df769979f8da1ee69c20735498a25d30a
Farzad FARID <farzy@farzy.org>
2020-05-16 16:20:12 +0200

    Some string formating

--- training.py
+++ training.py
@@ -1,5 +1,15 @@
+def strings():
+    """Training on strings"""
+    print("{0}, {1}, {2}".format('a', 'b', 'c'))
+    print("{}, {}, {}".format('a', 'b', 'c'))
+    print("{1}, {0}, {2}".format('a', 'b', 'c'))
+    print("{2}, {1}, {0}".format(*'abc'))
+    print("{0}{1}{0}".format('abra', 'cad'))
+
+
 def main():
-    pass
+    """The main program call the training sessions"""
+    strings()
 
 
 if __name__ == '__main__':
