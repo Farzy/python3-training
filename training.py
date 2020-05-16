3c34977853df241501565d027fb94e709b625e92
Farzad FARID <farzy@farzy.org>
2020-05-16 16:42:15 +0200

    Add global documentation

--- training.py
+++ training.py
@@ -1,3 +1,16 @@
+"""My training scripts
+
+    I use this project to practice on Python 3.8.
+
+    Author: Farzad FARID
+
+    References:
+        - Python 3.8 tutorial
+        - Python 3.8 library
+        - Python 3.8 reference
+"""
+
+
 def strings():
     """Training on strings"""
     print("{0}, {1}, {2}".format('a', 'b', 'c'))
