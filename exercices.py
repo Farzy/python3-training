d1c5a20263cf74c449957a2da7135f2f7ae140dc
Farzad FARID <farzy@farzy.org>
2020-05-17 23:48:57 +0200

    Looping

--- exercices.py
+++ exercices.py
@@ -490,3 +490,23 @@ def data_structures():
     print("dict from tuples:", dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
 
     print("dict comprehension", {x: x ** 2 for x in range(6)})
+
+    # Looping
+    knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+    for k, v in knights.items():
+        print(k, v)
+
+    for i, v in enumerate(['tic', 'tac', 'toe']):
+        print(i, v)
+
+    questions = ['name', 'quest', 'favorite color']
+    answers = ['lancelot', 'the holy grail', 'blue']
+    for q, a in zip(questions, answers):
+        print("What is your {0}?  It is {1}".format(q, a))
+
+    for i in reversed(range(1, 10, 2)):
+        print(i)
+
+    basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+    for f in sorted(set(basket)):
+        print(f)
