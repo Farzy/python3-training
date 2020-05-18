bcddef758ac6ae5b425fdf5ee217590428492f69
Farzad FARID <farzy@farzy.org>
2020-05-18 23:21:55 +0200

    Add fibo module

---
+++ fibo.py
@@ -0,0 +1,30 @@
+"""
+Fibonacci number module
+"""
+
+
+def fib(n: int):
+    """Write Fibonacci series up to n
+
+    :param n:
+    :return:
+    """
+    a, b = 1, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+def fib2(n: int) -> list:
+    """Return Fibonacci series up to n
+
+    :param n:
+    :return:
+    """
+    result = []
+    a, b = 1, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
