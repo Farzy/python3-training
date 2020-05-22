0d9f11bcf16c42e6d1a5c26d4bcdfbb8e3a708f8
Farzad FARID <farzy@farzy.org>
2020-05-22 22:49:50 +0200

    Add a practise on list comprehension

--- training.py
+++ training.py
@@ -9,7 +9,6 @@
         - Python 3.8 library
         - Python 3.8 reference
 """
-
 from sys import argv
 import exercices
 
@@ -27,15 +26,22 @@ def main():
     else:
         # Extract functions whose name does not start with '_' from module
         functions = filter(
-            lambda s: type(getattr(exercices, s)) == type(main) and not s.startswith('_'),
+            lambda sym: type(getattr(exercices, sym)) == type(main) and not sym.startswith('_'),
             dir(exercices)
         )
+        functions = list(functions)
+        # Do the same with list comprehension
+        functions2 = [
+            sym for sym in dir(exercices)
+            if type(getattr(exercices, sym)) == type(main) and not sym.startswith('_')
+        ]
+        assert functions == functions2
 
     print("List of exercises:")
     for func_name in functions:
-        f = getattr(exercices, func_name)   # Function object
-        name = f.__name__.capitalize()      # Function name
-        doc = (f.__doc__ or "NO DOCUMENTATION").splitlines()[0]     # First line of function documentation
+        f = getattr(exercices, func_name)  # Function object
+        name = f.__name__.capitalize()  # Function name
+        doc = (f.__doc__ or "NO DOCUMENTATION").splitlines()[0]  # First line of function documentation
         title_length = len(name) + len(doc)
         print()
         print("+" + "-" * (title_length + 4) + "+")
