32fdb2aed53c754704ebf4ef9fa3cffbba58df50
Farzad FARID <farzy@farzy.org>
2020-05-29 22:34:29 +0200

    Test scopes

--- exercices.py
+++ exercices.py
@@ -660,3 +660,28 @@ def exceptions():
         divide("2", "1")
     except TypeError as e:
         print("Caught exception:", e)
+
+def scopes():
+    """Experiment with scopes"""
+    def scope_test():
+        def do_local():
+            spam = "local spam"
+
+        def do_nonlocal():
+            nonlocal spam
+            spam = "nonlocal spam"
+
+        def do_global():
+            global spam
+            spam = "global spam"
+
+        spam = "test spam"
+        do_local()
+        print("After local assignment:", spam)
+        do_nonlocal()
+        print("After nonlocal assignment:", spam)
+        do_global()
+        print("After global assignment:", spam)
+
+    scope_test()
+    print("In global scope:", spam)
