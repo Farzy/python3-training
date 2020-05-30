77b003f8ac9132b250cb9abc960aa5f93ead2738
Farzad FARID <farzy@farzy.org>
2020-05-30 23:31:25 +0200

    Iterator and Generator

--- exercices.py
+++ exercices.py
@@ -879,3 +879,31 @@ def classes():
     assert issubclass(DerivedClass, BaseClass)
     assert not issubclass(BaseClass, DerivedClass)
     assert issubclass(BaseClass, object)
+
+    class Reverse:
+        """Iterator for looping over q sequence"""
+        def __init__(self, data):
+            self.data = data
+            self.index = len(data)
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.index == 0:
+                raise StopIteration
+            self.index -= 1
+            return self.data[self.index]
+
+    rev = Reverse('spam')
+    print("iter(rev) =", iter(rev))
+    for char in rev:
+        print(char)
+
+    def reverse(data):
+        """Generator function"""
+        for index in range(len(data)-1, -1, -1):
+            yield data[index]
+
+    for char in reverse('golf'):
+        print(char)
