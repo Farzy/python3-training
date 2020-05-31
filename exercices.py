13ab3459929fedbdcca4b088555aff9c104a5a32
Farzad FARID <farzy@farzy.org>
2020-05-31 19:33:47 +0200

    Add threading

--- exercices.py
+++ exercices.py
@@ -1106,6 +1106,30 @@ def stdlib_tour2():
         start += extra_size + comp_size     # Skip to the next header
 
 
+    import threading, zipfile
+
+    class AsyncZip(threading.Thread):
+        """Asynchronous file zipper"""
+
+        def __init__(self, infile, outfile):
+            threading.Thread.__init__(self)
+            self.infile = infile
+            self.outfile = outfile
+
+        def run(self):
+            f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+            f.write(self.infile)
+            f.close()
+            print("Finished background zip of:", self.infile)
+
+    background = AsyncZip('exercices.py', 'myarchive.zip')
+    background.start()
+    print("The main program continues to run in foreground.")
+
+    background.join()  # Wait for the background task to finish
+    print("Main program waited until background was done.")
+
+
 #########################################################################################
 # This section must be executed by calling exercises.py directly from the command line
 #########################################################################################
